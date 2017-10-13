3306f93e5b1b06364e0273216c907027d404ccae
Vista <retail3210@gmail.com>
2017-10-14 02:28:58 +0900

    Add entry point (index.js)

---
+++ index.js
@@ -0,0 +1,9 @@
+const Bot = require('./lib/Bot');
+const Channel = require('./lib/Channel');
+const ConsoleChannel = require('./lib/ConsoleChannel');
+
+module.exports = {
+    Bot,
+    Channel,
+    ConsoleChannel,
+};
